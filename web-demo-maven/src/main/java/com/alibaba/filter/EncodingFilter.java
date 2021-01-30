56b27fb18e94bf2ae58aacb14925a78eab4c94be
wangmengyang <193913232@qq.com>
2021-01-30 16:24:28 +0800

    提交代码

--- web-demo-maven/src/main/java/com/alibaba/filter/EncodingFilter.java
+++ web-demo-maven/src/main/java/com/alibaba/filter/EncodingFilter.java
@@ -1,7 +1,10 @@
 package com.alibaba.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Enumeration;
 
 public class EncodingFilter implements Filter {
 
@@ -11,6 +14,12 @@ public class EncodingFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        Enumeration<String> initParameterNames = filterConfig.getInitParameterNames();
+        while (initParameterNames.hasMoreElements()){
+            String s = initParameterNames.nextElement();
+            System.out.println("Filter init name "+s);
+            System.out.println("Filter init value "+filterConfig.getInitParameter(s));
+        }
         System.out.println("过滤器初始化");
     }
 
@@ -18,7 +27,17 @@ public class EncodingFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletRequest.setCharacterEncoding("utf-8"); //将编码改为utf-8
         servletResponse.setContentType("text/html;charset=utf-8");
+        System.out.println("EncodingFilter执行前！！！");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        Cookie[] cookies = httpServletRequest.getCookies();
+        for (Cookie cookie:cookies) {
+            String name = cookie.getName();
+            System.out.println("Cookie Name "+name);
+            String value = cookie.getValue();
+            System.out.println("Cookie Value "+value);
+        }
         filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("EncodingFilter执行后！！！");
     }
 
     @Override
---
+++ web-demo-maven/src/main/java/com/alibaba/utils/CookieUtil.java
@@ -0,0 +1,4 @@
+package com.alibaba.utils;
+
+public class CookieUtil {
+}
